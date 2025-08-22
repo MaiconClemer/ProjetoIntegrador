b5b60706dbaf97d12ae20984b84f79ee4a2ecc98
Maicon Clemer <maicon.clemer@gmail.com>
2025-08-22 20:08:34 -0300

    Front end com os elementos, falta ajustar tamanhos e posição.

---
+++ SenacCadPress/Imprimir.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace SenacCadPress
+{
+    partial class usuarioNovo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            richTextBox1 = new RichTextBox();
+            button1 = new Button();
+            fecharOProgramaToolStripMenuItem = new ToolStripMenuItem();
+            deslogarToolStripMenuItem = new ToolStripMenuItem();
+            sairToolStripMenuItem = new ToolStripMenuItem();
+            adicionarSaldoToolStripMenuItem = new ToolStripMenuItem();
+            todosToolStripMenuItem = new ToolStripMenuItem();
+            pesquisarClienteToolStripMenuItem = new ToolStripMenuItem();
+            verificarSaldoToolStripMenuItem = new ToolStripMenuItem();
+            imprimirToolStripMenuItem = new ToolStripMenuItem();
+            excluirToolStripMenuItem2 = new ToolStripMenuItem();
+            editarToolStripMenuItem2 = new ToolStripMenuItem();
+            label1 = new Label();
+            novoToolStripMenuItem2 = new ToolStripMenuItem();
+            excluirToolStripMenuItem1 = new ToolStripMenuItem();
+            editarToolStripMenuItem1 = new ToolStripMenuItem();
+            novoToolStripMenuItem1 = new ToolStripMenuItem();
+            clienteToolStripMenuItem = new ToolStripMenuItem();
+            excluirToolStripMenuItem = new ToolStripMenuItem();
+            editarToolStripMenuItem = new ToolStripMenuItem();
+            novoToolStripMenuItem = new ToolStripMenuItem();
+            usuárioToolStripMenuItem = new ToolStripMenuItem();
+            cadastrosToolStripMenuItem = new ToolStripMenuItem();
+            impressoraToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem1 = new ToolStripMenuItem();
+            button3 = new Button();
+            textBox1 = new TextBox();
+            menuStrip1 = new MenuStrip();
+            label2 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            label3 = new Label();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(12, 84);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(197, 360);
+            richTextBox1.TabIndex = 31;
+            richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(375, 342);
+            button1.Name = "button1";
+            button1.Size = new Size(187, 83);
+            button1.TabIndex = 30;
+            button1.Text = "Imprimir";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // fecharOProgramaToolStripMenuItem
+            // 
+            fecharOProgramaToolStripMenuItem.Name = "fecharOProgramaToolStripMenuItem";
+            fecharOProgramaToolStripMenuItem.Size = new Size(174, 22);
+            fecharOProgramaToolStripMenuItem.Text = "Fechar o programa";
+            // 
+            // deslogarToolStripMenuItem
+            // 
+            deslogarToolStripMenuItem.Name = "deslogarToolStripMenuItem";
+            deslogarToolStripMenuItem.Size = new Size(174, 22);
+            deslogarToolStripMenuItem.Text = "Deslogar";
+            // 
+            // sairToolStripMenuItem
+            // 
+            sairToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { deslogarToolStripMenuItem, fecharOProgramaToolStripMenuItem });
+            sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            sairToolStripMenuItem.Size = new Size(38, 20);
+            sairToolStripMenuItem.Text = "Sair";
+            // 
+            // adicionarSaldoToolStripMenuItem
+            // 
+            adicionarSaldoToolStripMenuItem.Name = "adicionarSaldoToolStripMenuItem";
+            adicionarSaldoToolStripMenuItem.Size = new Size(102, 20);
+            adicionarSaldoToolStripMenuItem.Text = "Adicionar Saldo";
+            // 
+            // todosToolStripMenuItem
+            // 
+            todosToolStripMenuItem.Name = "todosToolStripMenuItem";
+            todosToolStripMenuItem.Size = new Size(164, 22);
+            todosToolStripMenuItem.Text = "Todos";
+            // 
+            // pesquisarClienteToolStripMenuItem
+            // 
+            pesquisarClienteToolStripMenuItem.Name = "pesquisarClienteToolStripMenuItem";
+            pesquisarClienteToolStripMenuItem.Size = new Size(164, 22);
+            pesquisarClienteToolStripMenuItem.Text = "Pesquisar Cliente";
+            // 
+            // verificarSaldoToolStripMenuItem
+            // 
+            verificarSaldoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { pesquisarClienteToolStripMenuItem, todosToolStripMenuItem });
+            verificarSaldoToolStripMenuItem.Name = "verificarSaldoToolStripMenuItem";
+            verificarSaldoToolStripMenuItem.Size = new Size(92, 20);
+            verificarSaldoToolStripMenuItem.Text = "Verificar saldo";
+            // 
+            // imprimirToolStripMenuItem
+            // 
+            imprimirToolStripMenuItem.Name = "imprimirToolStripMenuItem";
+            imprimirToolStripMenuItem.Size = new Size(65, 20);
+            imprimirToolStripMenuItem.Text = "Imprimir";
+            // 
+            // excluirToolStripMenuItem2
+            // 
+            excluirToolStripMenuItem2.Name = "excluirToolStripMenuItem2";
+            excluirToolStripMenuItem2.Size = new Size(109, 22);
+            excluirToolStripMenuItem2.Text = "Excluir";
+            // 
+            // editarToolStripMenuItem2
+            // 
+            editarToolStripMenuItem2.Name = "editarToolStripMenuItem2";
+            editarToolStripMenuItem2.Size = new Size(109, 22);
+            editarToolStripMenuItem2.Text = "Editar";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 66);
+            label1.Name = "label1";
+            label1.Size = new Size(67, 15);
+            label1.TabIndex = 32;
+            label1.Text = "Mensagens";
+            // 
+            // novoToolStripMenuItem2
+            // 
+            novoToolStripMenuItem2.Name = "novoToolStripMenuItem2";
+            novoToolStripMenuItem2.Size = new Size(109, 22);
+            novoToolStripMenuItem2.Text = "Novo";
+            // 
+            // excluirToolStripMenuItem1
+            // 
+            excluirToolStripMenuItem1.Name = "excluirToolStripMenuItem1";
+            excluirToolStripMenuItem1.Size = new Size(109, 22);
+            excluirToolStripMenuItem1.Text = "Excluir";
+            // 
+            // editarToolStripMenuItem1
+            // 
+            editarToolStripMenuItem1.Name = "editarToolStripMenuItem1";
+            editarToolStripMenuItem1.Size = new Size(109, 22);
+            editarToolStripMenuItem1.Text = "Editar";
+            // 
+            // novoToolStripMenuItem1
+            // 
+            novoToolStripMenuItem1.Name = "novoToolStripMenuItem1";
+            novoToolStripMenuItem1.Size = new Size(109, 22);
+            novoToolStripMenuItem1.Text = "Novo";
+            // 
+            // clienteToolStripMenuItem
+            // 
+            clienteToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { novoToolStripMenuItem1, editarToolStripMenuItem1, excluirToolStripMenuItem1 });
+            clienteToolStripMenuItem.Name = "clienteToolStripMenuItem";
+            clienteToolStripMenuItem.Size = new Size(132, 22);
+            clienteToolStripMenuItem.Text = "Cliente";
+            // 
+            // excluirToolStripMenuItem
+            // 
+            excluirToolStripMenuItem.Name = "excluirToolStripMenuItem";
+            excluirToolStripMenuItem.Size = new Size(109, 22);
+            excluirToolStripMenuItem.Text = "Excluir";
+            // 
+            // editarToolStripMenuItem
+            // 
+            editarToolStripMenuItem.Name = "editarToolStripMenuItem";
+            editarToolStripMenuItem.Size = new Size(109, 22);
+            editarToolStripMenuItem.Text = "Editar";
+            // 
+            // novoToolStripMenuItem
+            // 
+            novoToolStripMenuItem.Name = "novoToolStripMenuItem";
+            novoToolStripMenuItem.Size = new Size(109, 22);
+            novoToolStripMenuItem.Text = "Novo";
+            // 
+            // usuárioToolStripMenuItem
+            // 
+            usuárioToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { novoToolStripMenuItem, editarToolStripMenuItem, excluirToolStripMenuItem });
+            usuárioToolStripMenuItem.Name = "usuárioToolStripMenuItem";
+            usuárioToolStripMenuItem.Size = new Size(132, 22);
+            usuárioToolStripMenuItem.Text = "Usuário";
+            // 
+            // cadastrosToolStripMenuItem
+            // 
+            cadastrosToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { usuárioToolStripMenuItem, clienteToolStripMenuItem, impressoraToolStripMenuItem });
+            cadastrosToolStripMenuItem.Name = "cadastrosToolStripMenuItem";
+            cadastrosToolStripMenuItem.Size = new Size(71, 20);
+            cadastrosToolStripMenuItem.Text = "Cadastros";
+            // 
+            // impressoraToolStripMenuItem
+            // 
+            impressoraToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { novoToolStripMenuItem2, editarToolStripMenuItem2, excluirToolStripMenuItem2 });
+            impressoraToolStripMenuItem.Name = "impressoraToolStripMenuItem";
+            impressoraToolStripMenuItem.Size = new Size(132, 22);
+            impressoraToolStripMenuItem.Text = "Impressora";
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(50, 20);
+            toolStripMenuItem1.Text = "Entrar";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(531, 147);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 26;
+            button3.Text = "Pesquisar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(282, 148);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(205, 23);
+            textBox1.TabIndex = 25;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { toolStripMenuItem1, cadastrosToolStripMenuItem, imprimirToolStripMenuItem, verificarSaldoToolStripMenuItem, adicionarSaldoToolStripMenuItem, sairToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Padding = new Padding(5, 2, 0, 2);
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 24;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(282, 130);
+            label2.Name = "label2";
+            label2.Size = new Size(28, 15);
+            label2.TabIndex = 27;
+            label2.Text = "CPF";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(404, 290);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(120, 23);
+            numericUpDown1.TabIndex = 33;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(394, 259);
+            label3.Name = "label3";
+            label3.Size = new Size(144, 15);
+            label3.TabIndex = 34;
+            label3.Text = "quantidade a ser Impressa";
+            // 
+            // usuarioNovo
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(numericUpDown1);
+            Controls.Add(richTextBox1);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(textBox1);
+            Controls.Add(menuStrip1);
+            Controls.Add(label2);
+            Name = "usuarioNovo";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+        private Button button1;
+        private ToolStripMenuItem fecharOProgramaToolStripMenuItem;
+        private ToolStripMenuItem deslogarToolStripMenuItem;
+        private ToolStripMenuItem sairToolStripMenuItem;
+        private ToolStripMenuItem adicionarSaldoToolStripMenuItem;
+        private ToolStripMenuItem todosToolStripMenuItem;
+        private ToolStripMenuItem pesquisarClienteToolStripMenuItem;
+        private ToolStripMenuItem verificarSaldoToolStripMenuItem;
+        private ToolStripMenuItem imprimirToolStripMenuItem;
+        private ToolStripMenuItem excluirToolStripMenuItem2;
+        private ToolStripMenuItem editarToolStripMenuItem2;
+        private Label label1;
+        private ToolStripMenuItem novoToolStripMenuItem2;
+        private ToolStripMenuItem excluirToolStripMenuItem1;
+        private ToolStripMenuItem editarToolStripMenuItem1;
+        private ToolStripMenuItem novoToolStripMenuItem1;
+        private ToolStripMenuItem clienteToolStripMenuItem;
+        private ToolStripMenuItem excluirToolStripMenuItem;
+        private ToolStripMenuItem editarToolStripMenuItem;
+        private ToolStripMenuItem novoToolStripMenuItem;
+        private ToolStripMenuItem usuárioToolStripMenuItem;
+        private ToolStripMenuItem cadastrosToolStripMenuItem;
+        private ToolStripMenuItem impressoraToolStripMenuItem;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private Button button3;
+        private TextBox textBox1;
+        private MenuStrip menuStrip1;
+        private Label label2;
+        private NumericUpDown numericUpDown1;
+        private Label label3;
+    }
+}
---
+++ SenacCadPress/UserControl1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace SenacCadPress
+{
+    partial class UserControl1
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(56, 72);
+            label1.Name = "label1";
+            label1.Size = new Size(28, 15);
+            label1.TabIndex = 0;
+            label1.Text = "CPF";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(56, 149);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Senha";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(69, 225);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Entrar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(56, 90);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(56, 167);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // UserControl1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "UserControl1";
+            Size = new Size(210, 314);
+            Load += UserControl1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+    }
+}
